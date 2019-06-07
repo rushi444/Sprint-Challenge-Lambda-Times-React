ee14adb1927c1e37cb794f91b45f009053495efa
rushi444 <rushi44@gmail.com>
2019-06-07 08:20:41 -0700

    installed yarn

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import styled from 'styled-components'; 
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+
+const
 
 const TopBar = () => {
   return (
